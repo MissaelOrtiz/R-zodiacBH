ac3f8f7f7430e9bce148ddb737fa1c4ef669c263
Missael Ortiz <ortiz.missael80@gmail.com>
2022-01-25 14:31:52 -0800

    changed img src attribute to correct file path

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -4,7 +4,7 @@ export default function ZodiacCard({ name, dates }) {
   return (
     <div className="zodiac-card">
       <h2>Zodiac card</h2>
-      <img alt={name} src={`/images/${name}`} />
+      <img alt={name} src={`./images/${name}.png`} />
       <span className="name">{name}</span>
       <span>{dates}</span>
     </div>
